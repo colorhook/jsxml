4da7ce0ae21a1b594284d066f27b0c11f8fd287a
colorhook <colorhook@gmail.com>
2011-07-10 00:23:14 +0800

    change the php proxy manner to YQL manner for cross-domain data exchange

--- examples/service.php
+++
@@ -1,4 +0,0 @@
-<?php
-$url = "http://api.t.sina.com.cn/trends/statuses.xml?trend_name=".urlencode("D2前端技术论坛")."&source=506163941";
-//$url = "http://api.t.sina.com.cn/trends/statuses/user_timeline.xml?screen_name=".urlencode("D2前端技术论坛")."&source=506163941";
-echo file_get_contents($url);
